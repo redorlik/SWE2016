460011fe31ab6d363e0f23e5a740787d836526bd
Anders Lehmann <redorlik@gmail.com>
2016-09-22 11:10:15 +0200

    simplify run_tests.sh script

--- RLE/run_tests.sh
+++ RLE/run_tests.sh
@@ -1,13 +1,13 @@
 
 export PATH=/usr/local/bin:$PATH
-python -mpip install -U pip
-python -mpip install -U virtualenv 
+# python -mpip install -U pip
+# python -mpip install -U virtualenv 
 
 
 virtualenv testvenv
 
 . testvenv/bin/activate
 
-python -mpip install -r RLE/requirements.txt
+pip install -r RLE/requirements.txt
 pytest RLE
 

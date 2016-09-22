1bfe0b0da761ec621311e5ce4a4b886271228370
Anders Lehmann <redorlik@gmail.com>
2016-09-22 08:38:17 +0200

    try differently run_tests.sh script

--- RLE/run_tests.sh
+++ RLE/run_tests.sh
@@ -1,12 +1,12 @@
 
-python -mpip install -U pip
-python -mpip install -U virtualenv 
+/usr/local/bin/python -mpip install -U pip
+/usr/local/bin/python -mpip install -U virtualenv 
 
-python -mpip install -r RLE/requirements.txt
 
 virtualenv testvenv
 
 . testenv/bin/activate
 
+python -mpip install -r RLE/requirements.txt
 pytest RLE
 

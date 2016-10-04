2df42c928f25bef0032d26d8dd396a75ff746bf6
Anders Lehmann <redorlik@gmail.com>
2016-10-04 10:17:41 +0200

    Halfway fixed problems with numbers. Created a problem with backslash

--- RLE/rle.py
+++ RLE/rle.py
@@ -3,11 +3,17 @@
 # Skal konvertere feks. 'kkkbb' til '3k2b'
 #
 #
+def compound(count,old):
+    if old.isdigit():
+        res = '%d\\%s' %(count,old)
+    else:
+        res = '%d%s' %(count,old)
+    return res
 
 def rle_encoder(message):
     if not message:
         return message
-    
+
     old = message[0]
     count = 1
     result = []
@@ -15,26 +21,34 @@ def rle_encoder(message):
         if ch == old:
             count += 1
         else:
-            result.append('%d%s' %(count,old))
+            result.append(compound(count,old))
             old = ch
             count = 1
-    result.append('%d%s' %(count,old))
+    result.append(compound(count,old))
     return ''.join(result)
 
 def rle_decoder(code):
     i = 0
+    j = 0
     result = []
-    for ch in code:
+    while j < len(code):
+        ch = code[j]
         if '0' <= ch <= '9':
             i = 10*i
             i += int(ch)
+        elif ch == '\\':
+            j += 1
+            ch = code[j]
+            result.append(i*ch)
+            i = 0
         else:
             result.append(i*ch)
             i = 0
+        j += 1
     return ''.join(result)
 
 if __name__ == '__main__':
     import sys
-
-    print rle_encoder(sys.argv[1])
-
+    a = rle_encoder(sys.argv[1])
+    import pdb;pdb.set_trace()
+    rle_decoder(a)
--- RLE/test_rle.py
+++ RLE/test_rle.py
@@ -8,7 +8,34 @@ def test_encode():
 
 def test_more():
     assert rle_encoder('#####') == '5#'
-    
+
+def test_encoder():
+    assert rle_encoder('kkkbb') == '3k2b'
+
+def test_encoder2():
+    assert rle_encoder('dkkkbb') == '1d3k2b'
+    assert rle_encoder('kkkkkkkkkkbb') == '10k2b'
+
+def test_encoder_tal():
+    assert rle_encoder('1111kkkbb') == '4\\13k2b'
+
+def test_encoder_space():
+    assert rle_encoder('kkk          bb') == '3k10 2b'
+
+def test_encoder_empty():
+    assert rle_encoder('') == ''
+    assert rle_encoder('0') == '1\\0'
+
+def test_decoder():
+    assert rle_decoder('2k3b') == 'kkbbb'
+
+def test_decoder_space():
+    assert rle_decoder('3k10 2b') == 'kkk          bb'
+    assert rle_decoder('10k2b') == 'kkkkkkkkkkbb'
+    assert rle_decoder('10\\3') == '3333333333'
+    assert rle_decoder('1\\0') == '0'
+    assert rle_decoder('1\\01\\1') == '01'
+
 @given(text())
 def test_encode_decode(input):
     assert rle_decoder(rle_encoder(input)) == input

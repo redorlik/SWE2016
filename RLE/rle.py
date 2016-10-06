833376a0e544e9781b3acf91a8ba504462f072c0
Anders Lehmann <redorlik@gmail.com>
2016-10-06 11:26:59 +0200

    Changed the interface between encoder and decoder

--- RLE/rle.py
+++ RLE/rle.py
@@ -1,14 +1,8 @@
 # Run length encoder :
 #
-# Skal konvertere feks. 'kkkbb' til '3k2b'
+# Skal konvertere feks. 'kkkbb' til [(3,'k'),(2,'b')]
 #
 #
-def compound(count,old):
-    if old.isdigit():
-        res = '%d\\%s' %(count,old)
-    else:
-        res = '%d%s' %(count,old)
-    return res
 
 def rle_encoder(message):
     if not message:
@@ -17,38 +11,25 @@ def rle_encoder(message):
     old = message[0]
     count = 1
     result = []
+    
     for ch in message[1:]:
         if ch == old:
             count += 1
         else:
-            result.append(compound(count,old))
+            result.append((count,old))
             old = ch
             count = 1
-    result.append(compound(count,old))
-    return ''.join(result)
+    result.append((count,old))
+    return result
 
 def rle_decoder(code):
-    i = 0
-    j = 0
-    result = []
-    while j < len(code):
-        ch = code[j]
-        if '0' <= ch <= '9':
-            i = 10*i
-            i += int(ch)
-        elif ch == '\\':
-            j += 1
-            ch = code[j]
-            result.append(i*ch)
-            i = 0
-        else:
-            result.append(i*ch)
-            i = 0
-        j += 1
+
+    result = [count*ch for count,ch in code]
     return ''.join(result)
 
 if __name__ == '__main__':
     import sys
     a = rle_encoder(sys.argv[1])
-    import pdb;pdb.set_trace()
-    rle_decoder(a)
+    #print a
+    #import pdb;pdb.set_trace()
+    print rle_decoder(a)
--- RLE/test_rle.py
+++ RLE/test_rle.py
@@ -2,39 +2,39 @@ from rle import rle_encoder,rle_decoder
 from hypothesis import given
 from hypothesis.strategies import text
 
-def test_encode():
-    assert rle_encoder('aa') == '2a'
-    assert rle_encoder('wwwwwbbb') == '5w3b'
+def test_simple_encode():
+    assert rle_encoder('aa') == [(2,'a')]
+    assert rle_encoder('wwwwwbbb') == [(5,'w'),(3,'b')]
 
 def test_more():
-    assert rle_encoder('#####') == '5#'
+    assert rle_encoder('#####') == [(5,'#')]
 
 def test_encoder():
-    assert rle_encoder('kkkbb') == '3k2b'
+    assert rle_encoder('kkkbb') == [(3,'k'),(2,'b')]
 
 def test_encoder2():
-    assert rle_encoder('dkkkbb') == '1d3k2b'
-    assert rle_encoder('kkkkkkkkkkbb') == '10k2b'
+    assert rle_encoder('dkkkbb') == [(1,'d'),(3,'k'),(2,'b')]
+    assert rle_encoder('kkkkkkkkkkbb') == [(10,'k'),(2,'b')]
 
 def test_encoder_tal():
-    assert rle_encoder('1111kkkbb') == '4\\13k2b'
+    assert rle_encoder('1111kkkbb') == [(4,'1'),(3,'k'),(2,'b')]
 
 def test_encoder_space():
-    assert rle_encoder('kkk          bb') == '3k10 2b'
+    assert rle_encoder('kkk          bb') == [(3,'k'),(10,' '),(2,'b')]
 
 def test_encoder_empty():
     assert rle_encoder('') == ''
-    assert rle_encoder('0') == '1\\0'
+    assert rle_encoder('0') == [(1,'0')]
 
 def test_decoder():
-    assert rle_decoder('2k3b') == 'kkbbb'
+    assert rle_decoder([(2,'k'),(3,'b')]) == 'kkbbb'
 
 def test_decoder_space():
-    assert rle_decoder('3k10 2b') == 'kkk          bb'
-    assert rle_decoder('10k2b') == 'kkkkkkkkkkbb'
-    assert rle_decoder('10\\3') == '3333333333'
-    assert rle_decoder('1\\0') == '0'
-    assert rle_decoder('1\\01\\1') == '01'
+    assert rle_decoder([(3,'k'),(10,' '),(2,'b')]) == 'kkk          bb'
+    assert rle_decoder([(10,'k'),(2,'b')]) == 'kkkkkkkkkkbb'
+    assert rle_decoder([(10,'3')]) == '3333333333'
+    assert rle_decoder([(1,'0')]) == '0'
+    assert rle_decoder([(1,'0'),(1,'1')]) == '01'
 
 @given(text())
 def test_encode_decode(input):
